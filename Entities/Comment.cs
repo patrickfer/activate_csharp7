3cf26a8071ea01e824bb21c3133225bc95efa793
Patrick Fernando <patrickfernando1234@gmail.com>
2022-12-13 16:53:12 -0300

    fist commit

---
+++ Entities/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+        
+    }   
+}
